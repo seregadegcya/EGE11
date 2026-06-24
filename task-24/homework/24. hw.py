7145bb682893b48f3e21a1984b603f89f6c29ea5
seregadegcya <sergiodegtyarev@icloud.com>
2026-06-24 15:23:33 +0200

    /

--- task-24/homework/24. hw.py
+++ task-24/homework/24. hw.py
@@ -119,13 +119,48 @@ from string import digits, ascii_uppercase
 #         element = element[:-1]
 #     max_len=max(len(element), max_len)
 # print(max_len)
-from string import digits, ascii_uppercase
-with open("2422.txt") as f:
-    data = f.readline()
-good = digits + ascii_uppercase
-alph = digits + ascii_uppercase
-good = alph[:12]
-n="02468A"
-left=max_len=0
-for right in range(len(data)):
-    if data[right] in good:
+# from string import digits, ascii_uppercase
+# with open("2422.txt") as f:
+#     data = f.readline()
+# good = digits + ascii_uppercase
+# alph = digits + ascii_uppercase
+# good = alph[:12]
+# n="02468A"
+# left=max_len=0
+# for right in range(len(data)):
+#     if data[right] in good:
+# Текстовый файл состоит из десятичных цифр и заглавных букв латинского алфавита.
+# Определите в этом файле последовательность
+# идущих подряд символов, представляющих собой запись максимального чётного 14-ричного числа.
+# В ответе запишите количество символов (значащих цифр в записи числа) в этой последовательности.
+# from string import digits, ascii_uppercase
+# with open("2424.txt") as f:
+#     data = f.readline()
+# alph = digits + ascii_uppercase
+# good = alph[:14]
+# n="02468AC"
+# cnt=max_len=0
+# for i in range(len(data)):
+#     if data[i] in good:
+#         cnt+=1
+#         if data[i] in n:
+#             max_len=max(max_len, cnt)
+#     else:
+#         cnt=0
+# print(max_len)
+# from string import digits, ascii_uppercase
+# with open("2424.txt") as f:
+#     data = f.readline()
+# alph = digits + ascii_uppercase
+# good = alph[:14]
+# bad=alph[14:]
+# n="02468AС"
+# max_len=0
+# for sym in bad:
+#     data = data.replace(sym, " ")
+# for element in data.split():
+#     while element and element[-1] not in n:
+#         element = element[:-1]
+#     if element:
+#         max_len = max(len(element), max_len)
+# print(max_len)
